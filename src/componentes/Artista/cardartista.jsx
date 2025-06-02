1b8dba90b65cfe8ac6c201b12c133b6007e211b4
Apolo0013 <148099939+Apolo0013@users.noreply.github.com>
2025-06-02 12:24:07 -0300

    3° modelo

--- src/componentes/Artista/cardartista.jsx
+++ src/componentes/Artista/cardartista.jsx
@@ -1,30 +1,53 @@
 import './cardartista.css'
 import ImgEx from '../../img/ex.jpg'
+import { useEffect } from 'react'
 
-function CardArtista() {
+function CardArtista(props) {
+    useEffect(() => {
+        console.log(props.profissoes)
+    })
     return (
         < div className="CardArtista">
             <div className="infoArtista">
                 <img src={ImgEx} alt="" />
                 <span>
                     <div>
-                        <h2>Idade:</h2>
-                        <h3>30</h3>
+                        <h2>Idade</h2>
+                        
+                        <h3>{
+                            //string
+                            props.idade
+                        }</h3>
                     </div>
                     <div>
                         <h2>Profissão</h2>
                         <ul>
-                            <li>Composito</li>
-                            <li>Artista</li>
-                            <li>Canto</li>
+                            {
+                                //lista
+                                props.profissoes.map((valor, i) => (
+                                    <li key={i}>{valor}</li>
+                                ))
+                            }
                         </ul>
                     </div>
                 </span>
             </div>
             <div className="bio">
-                <h2>Apolonio Guilherme Lima da Silva</h2>
                 <div>    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, architecto dolores eos, atque quaerat eum magni necessitatibus voluptatibus dolore dolorem, voluptate ad optio vel quidem dolorum ipsum delectus minima repellat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, excepturi in! Fugiat, iste culpa sit rerum dolore harum excepturi tempore quaerat sunt ut maxime totam dolores provident velit illum quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, dolores possimus minima tempora alias nisi velit dolorum officia explicabo ex ad similique officiis quo ipsa perspiciatis laboriosam repellat ullam quibusdam!</p>
+                    <h2>{props.nome}</h2>
+                    <section className="biografia">
+                        <p>{props.biografia}</p>
+                    </section>
+                    <section className="obras">
+                        <section>
+                            <p>{props.textos_obras[0]}</p>
+                            <div><img src={props.imgobras[0]} alt="img" /></div>
+                        </section>
+                        <section>
+                            <p>{props.textos_obras[1]}</p>
+                            <div><img src={props.imgobras[1]} alt="img" /></div>
+                        </section>
+                    </section>
                 </div>
             </div>
         </div >
--- src/componentes/Artista/conteinerartista.jsx
+++ src/componentes/Artista/conteinerartista.jsx
@@ -2,17 +2,23 @@ import './conteinerartista.css'
 import imgfecha from '../../img/fechaimg.webp'
 import CardArtista from './cardartista.jsx'
 
+//artista imagens obras e artista.
+import fds from '../../img/img1.jpg'
+import fds2 from '../../img/D_Q_NP_717851-MLU50424885263_062022-O.webp'
+
 function ConteinerArtista(props) {
     return (
         <div className="ConteinerArtista">
             <img src={imgfecha} onClick={props.fecha} alt="" />
             <div className="ConteinerCards">
-                <CardArtista></CardArtista>
-                <CardArtista></CardArtista>
-                <CardArtista></CardArtista>
-                <CardArtista></CardArtista>
-                <CardArtista></CardArtista>
-                <CardArtista></CardArtista>
+                <CardArtista
+                    nome='Abdias do Nascimento'
+                    idade='(1914 - 2011)'
+                    profissoes={['ativista', 'político', 'poeta', 'professor', 'dramaturgo']}
+                    biografia='Abdias do Nascimento foi uma das figuras mais importantes na luta contra o racismo no Brasil. Fundador do Teatro Experimental do Negro, em 1944, ele atuou como militante pela valorização da cultura negra nas artes e na política. No período da ditadura militar, denunciou o racismo brasileiro em organismos internacionais como a ONU. Na política, foi deputado federal e senador, defendendo pautas como cotas raciais e a criação do Dia da Consciência Negra. Sua vida é símbolo de resistência e ativismo antirracista.'
+                    textos_obras={['"O Quilombismo" – Obra teórica que propõe uma sociedade baseada nos valores dos quilombos, como solidariedade e resistência.', '"Sortilégio" – Peça teatral que denuncia o racismo e exalta a identidade negra']}
+                    imgobras={[fds, fds2]}
+                ></CardArtista>
             </div>
         </div>
     )  
